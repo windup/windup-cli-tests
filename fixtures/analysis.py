de4006e01b235a8e19d3a8e55f4ec6f7e6c1a5da
Alejandro Brugarolas <abrugaro@redhat.com>
2023-02-21 09:42:19 +0100

    fix pr
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- fixtures/analysis.py
+++ fixtures/analysis.py
@@ -4,6 +4,6 @@ import pytest
 
 @pytest.fixture(scope="session")
 def analysis_data():
-    with open('../data/analysis.json', 'r') as file:
+    with open('data/analysis.json', 'r') as file:
         json_list = json.load(file)
     return {value['app_name']: value for value in json_list}
