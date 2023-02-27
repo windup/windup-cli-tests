83d51df6dde5603822e2c679112c3de513806c3f
Alejandro Brugarolas <abrugaro@redhat.com>
2023-02-27 11:55:28 +0100

    add standard analyses
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,39 +1,24 @@
 import json
-import os
 import subprocess
 
 import pytest
-from bs4 import BeautifulSoup
+
+from utils.command import build_command
+from utils.report import assert_story_points_from_report_file
 
 
 @pytest.mark.parametrize('app_name', json.load(open("data/analysis.json")))
 def test_standard_analysis(app_name, analysis_data):
     application_data = analysis_data[app_name]
 
-    windup_path = os.getenv('WINDUP_CLI_PATH')
-    report_path = os.getenv('REPORT_OUTPUT_PATH')
-    binary_path = os.path.join(
-        os.getenv('PROJECT_PATH'),
-        "fixtures/applications",
-        application_data['file_name']
+    command = build_command(
+        application_data['file_name'],
+        application_data['source'],
+        application_data['target']
     )
 
-    command = windup_path + ' --output ' + report_path + ' --input ' + binary_path + ' --target ' + \
-              application_data['target'] + ' --overwrite -b '
-
-    source = application_data['source']
-    if source:
-        command += ' --source ' + source
-
-    print(command)
     output = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8').stdout
 
     assert 'Report created' in output
 
-    with open(report_path + "/index.html") as file:
-        html_report = file.read()
-    parsed_report = BeautifulSoup(html_report, 'html.parser')
-
-    story_points = int(parsed_report.find('span', class_='points').text)
-
-    assert story_points == application_data['story_points']
+    assert_story_points_from_report_file(application_data['story_points'])
