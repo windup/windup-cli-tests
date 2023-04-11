a6b06b345e0efda7d2450ce22c26cf26df752ef8
Alejandro Brugarolas <abrugaro@redhat.com>
2023-04-11 12:39:18 +0200

    Add advanced options tests
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- utils/report.py
+++ utils/report.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 from bs4 import BeautifulSoup
@@ -13,3 +14,28 @@ def assert_story_points_from_report_file(story_points):
     report_story_points = int(parsed_report.find('span', class_='points').text)
 
     assert report_story_points == story_points
+
+
+def assert_valid_csv(csv_file_path, **kwargs):
+    """
+        Asserts that the given CSV file is valid.
+        A CSV file is considered valid if it exists and all of its rows have the same number of columns.
+
+        Args:
+            csv_file_path (str): The file path of the CSV file to validate.
+            **kwargs (str): Optional keyword arguments.
+                delimiter (str): The delimiter used in the CSV file. Defaults to ','.
+
+        Raises:
+            AssertionError: If the CSV file does not exist or if its rows have different numbers of columns.
+
+        Returns:
+            None.
+    """
+    assert os.path.exists(csv_file_path) is True
+    delimiter = kwargs.get('delimiter', ',')
+
+    with open(csv_file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=delimiter)
+        columns_number = len(next(csv_reader))
+        assert all(len(row) == columns_number for row in csv_reader) is True
